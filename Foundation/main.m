15fce410c4f60ea507a3564f81a97f556121804c
Damian Martinez <damian-git@malachiarts.com>
2015-02-13 11:56:02 -0800

    Initial Commit

---
+++ Foundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Foundation
+//
+//  Created by d on 13/2/15.
+//  Copyright (c) 2015 d. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
