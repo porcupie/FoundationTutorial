6dab06d45bef01b8c26dfb8ac38bb7dc58c5f5ba
Damian Martinez <damian-git@malachiarts.com>
2015-02-13 12:00:03 -0800

    Following along on https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/DefiningClasses/DefiningClasses.html

---
+++ Foundation/XYZPerson.h
@@ -0,0 +1,13 @@
+//
+//  XYZPerson.h
+//  Foundation
+//
+//  Created by d on 13/2/15.
+//  Copyright (c) 2015 d. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZPerson : NSObject
+
+@end
---
+++ Foundation/XYZPerson.m
@@ -0,0 +1,13 @@
+//
+//  XYZPerson.m
+//  Foundation
+//
+//  Created by d on 13/2/15.
+//  Copyright (c) 2015 d. All rights reserved.
+//
+
+#import "XYZPerson.h"
+
+@implementation XYZPerson
+
+@end
